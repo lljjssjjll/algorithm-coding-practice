ef3c5cb92800bc61089e3ad531afb143a3616bc3
Junseok Lee <31983252+lljjssjjll@users.noreply.github.com>
2018-06-19 10:43:15 +0900

    0123456789 숫자 사용 문제 풀이 추가

--- src/main/OnlyOnceUsed.java
+++ src/main/OnlyOnceUsed.java
@@ -3,15 +3,12 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Stack;
 
 public class OnlyOnceUsed {
 	/*
 	0~9까지의 문자로 된 숫자를 입력 받았을 때,
-	이 입력 값이 0~9까지의 숫자가 각각 한 번 씩만 사용된 것인지 확인하는 함수를 구하시오.
+	이 입력 값이 0~9까지의 숫자가 각각 한 번 씩만(최소) 사용된 것인지 확인하는 함수를 구하시오.
 	sample inputs: 0123456789 01234 01234567890 6789012345 012322456789
 	sample outputs: true false false true false
 	*/
@@ -21,26 +18,37 @@ public class OnlyOnceUsed {
 	}
 	
 	public static void isOnlyOnceUsed() throws IOException {
-		boolean isOnlyOnceUsed = true;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("0 ~ 9자리 숫자를 입력하세요. : ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String iStr = br.readLine();
 		br.close();
-		List<String> numList = new ArrayList<String>(Arrays.asList("0","1","2","3","4","5","6","7","8","9"));
-		Stack<String> numStack = (Stack<String>) numList;
-		char[] charArr = iStr.toCharArray();
-		for(int i = 0; i < charArr.length - 1; i++) {
-			for(int j = i + 1; j < charArr.length; j++) {
-				if(charArr[i] == charArr[j]) {
-					isOnlyOnceUsed = false;
-					break;
-				}
-			}
-			if(!isOnlyOnceUsed) {
+		
+		Stack<String> numStack = new Stack<String>();
+		for(int i = 0; i < 10; i++) {
+			numStack.push(String.valueOf(i));
+		}
+		
+		boolean isOnlyOnceUsed = true;
+		
+		int initStackSize = numStack.size();
+		int cnt = 0;
+		
+		while(true) {
+			String num = numStack.peek();
+			cnt++;
+			if(iStr.contains(num) && cnt <= initStackSize) {
+				numStack.pop();
+				iStr = iStr.replaceFirst(num, "");
+				if(numStack.size() == 0) break;
+			} else {
+				isOnlyOnceUsed = false;
 				break;
 			}
 		}
-		System.out.printf("0123456789 숫자가 한번씩만 사용되었는가? (true or false) : %b\n", isOnlyOnceUsed);
+		
+		if(iStr.length() != 0) isOnlyOnceUsed = false;
+		
+		System.out.printf("0123456789 숫자가 각각 최소 한번씩 사용되었는가? (true or false) : %b\n", isOnlyOnceUsed);
 	}
 
 }
